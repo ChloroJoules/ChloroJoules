38a4cbd872594358b5317e9c36fad5f42480bad1
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2025-08-01 11:45:43 +0100

    chlorojoules: implement floral enervator generation

--- src/main/java/io/github/chlorojoules/machine/CJMachineEnervator.java
+++ src/main/java/io/github/chlorojoules/machine/CJMachineEnervator.java
@@ -1,16 +1,21 @@
 package io.github.chlorojoules.machine;
 
-import io.github.chlorojoules.CJMod;
-import io.github.chlorojoules.CJRarity;
-import io.github.chlorojoules.CJRarityInfo;
-import io.github.chlorojoules.CJTankVolume;
+import io.github.chlorojoules.*;
 import io.github.chlorojoules.block.tileentity.CJTileEntityMachineBase;
-import io.github.chlorojoules.item.CJIItemSocket;
+import net.minecraft.common.block.Block;
+import net.minecraft.common.block.Blocks;
+import net.minecraft.common.block.children.*;
+import net.minecraft.common.entity.inventory.IInventory;
 import net.minecraft.common.item.ItemStack;
 import net.minecraft.common.util.i18n.StringTranslate;
+import net.minecraft.common.world.World;
+
+import static io.github.chlorojoules.block.tileentity.CJTileEntityMachineBase.machineEntity;
 
 @SuppressWarnings("unused")
 public class CJMachineEnervator implements CJIMachine {
+	private BlockFlower[] adjacentFlowers;
+
 	@Override
 	public void updateMachine(CJTileEntityMachineBase machineEntity) {
 		StringTranslate translate = StringTranslate.getInstance();
@@ -29,14 +34,14 @@ public class CJMachineEnervator implements CJIMachine {
 		machineEntity.errorMessage = null;
 		machineEntity.isWarning = false;
 		machineEntity.isPassive = false;
-		machineEntity.operationLength = 100;
+		machineEntity.operationLength = 75;
 
-		if(catalyst.current == 0) {
-			machineEntity.operationTicks = 0;
-			machineEntity.errorMessage =
-					translate.translateKey("message.cj_no_optional");
-
-			return;
+		if(adjacentFlowers == null) {
+			onNeighbourChange(
+					machineEntity.worldObj,
+					machineEntity.xCoord,
+					machineEntity.yCoord,
+					machineEntity.zCoord);
 		}
 
 		if(jewelStack == null) {
@@ -46,5 +51,92 @@ public class CJMachineEnervator implements CJIMachine {
 
 			return;
 		}
+
+		if(catalyst.current == 0) {
+			machineEntity.operationTicks = 0;
+			machineEntity.errorMessage =
+					translate.translateKey("message.cj_no_optional");
+
+			return;
+		}
+
+		int efficiency;
+		int consumption;
+		if(catalyst.fluidID == CJMod.fuelFluid) {
+			efficiency = 3;
+			consumption = 0;
+		}
+		else {
+			machineEntity.operationTicks = 0;
+			machineEntity.errorMessage =
+					translate.translateKey("message.cj_bad_catalyst");
+
+			return;
+		}
+
+		if(catalyst.current < consumption) {
+			machineEntity.operationTicks = 0;
+			machineEntity.errorMessage =
+					translate.translateKey("message.cj_no_optional");
+
+			return;
+		}
+
+		CJRarity rarity = CJRarityInfo.getJewelRarity(jewelStack.getItemID());
+		machineEntity.operationLength -=
+				CJRarityInfo.getRarityTimeScale(rarity);
+
+		int flowerCount = 0;
+		for(BlockFlower flower : adjacentFlowers) {
+			if(flower == null) continue;
+
+			flowerCount++;
+
+			if(flower instanceof BlockGoldenFlower) {
+				machineEntity.operationLength -= 9;
+				efficiency += 8;
+			}
+			else if(flower instanceof BlockSilverFlower) {
+				machineEntity.operationLength -= 6;
+				efficiency += 4;
+			}
+			else {
+				machineEntity.operationLength -= 2;
+				efficiency += 2;
+			}
+		}
+
+		if(flowerCount == 0) {
+			machineEntity.operationTicks = 0;
+			machineEntity.errorMessage =
+					translate.translateKey("message.cj_no_flowers");
+
+			return;
+		}
+
+		efficiency += CJRarityInfo.getRarityPowerScale(rarity);
+
+		machineEntity.operationTicks++;
+
+		if(machineEntity.operationTicks >= machineEntity.operationLength) {
+			jewelStack.damageItem(1, null, true);
+			output.addFluid(CJMod.fuelFluid, efficiency, false);
+			catalyst.removeFluid(catalyst.fluidID, consumption, false);
+			machineEntity.operationTicks = 0;
+		}
+	}
+
+	@Override
+	public void onNeighbourChange(World world, int x, int y, int z) {
+		adjacentFlowers = new BlockFlower[4];
+
+		Block block = Blocks.BLOCKS_LIST[world.getBlockId(x + 1, y, z)];
+		if(block instanceof BlockFlower flower) adjacentFlowers[0] = flower;
+		block = Blocks.BLOCKS_LIST[world.getBlockId(x - 1, y, z)];
+		if(block instanceof BlockFlower flower) adjacentFlowers[0] = flower;
+		block = Blocks.BLOCKS_LIST[world.getBlockId(x, y, z + 1)];
+		if(block instanceof BlockFlower flower) adjacentFlowers[0] = flower;
+		block = Blocks.BLOCKS_LIST[world.getBlockId(x, y, z - 1)];
+		if(block instanceof BlockFlower flower) adjacentFlowers[0] = flower;
 	}
 }
