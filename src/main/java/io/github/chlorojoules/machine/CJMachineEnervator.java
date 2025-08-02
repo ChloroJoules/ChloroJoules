7f25ae14b3882e183a8f52d391cbec4b3c94cc8a
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2025-08-02 13:00:43 +0100

    chlorojoules: update enervator to not work at night or in hell

--- src/main/java/io/github/chlorojoules/machine/CJMachineEnervator.java
+++ src/main/java/io/github/chlorojoules/machine/CJMachineEnervator.java
@@ -79,6 +79,22 @@ public class CJMachineEnervator implements CJIMachine {
 			return;
 		}
 
+		if(machineEntity.worldObj.worldInfo.getDimension() == -1) {
+			machineEntity.operationTicks = 0;
+			machineEntity.errorMessage =
+					translate.translateKey("message.cj_in_hell");
+
+			return;
+		}
+
+		if(!(machineEntity.worldObj.getWorldTime() < 12000L)) {
+			machineEntity.operationTicks = 0;
+			machineEntity.errorMessage =
+					translate.translateKey("message.cj_no_sky");
+
+			return;
+		}
+
 		CJRarity rarity = CJRarityInfo.getJewelRarity(jewelStack.getItemID());
 		machineEntity.operationLength -=
 				CJRarityInfo.getRarityTimeScale(rarity);
